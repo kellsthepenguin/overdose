1bac8d0b2d7184a9275d30462c25a56aa9fe66c3
Kim Jioo <jwkim101201@gmail.com>
2022-09-20 07:47:29 +0900

    feat: create landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import Topbar from './components/Topbar'
+import img from './assets/solid-color-image.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
 export default function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello world!
-    </h1>
+    <div>
+      <Topbar />
+      <div className='absolute -translate-y-1/2 -translate-x-1/2 left-1/2 top-1/2 text-left'>
+        <div className='flex row gap-7 items-center'>
+          <div>
+            <p className='font-xl'>당신의 대화는 보호받아야 하니까.</p>
+            <p className='text-5xl font-bold font-mono pb-2'>Overdose.</p>
+            <button type='button' className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2 mr-2 mb-2m focus:outline-none'>
+              시작하기 &nbsp;
+              <FontAwesomeIcon icon={faChevronRight} />
+            </button>
+          </div>
+
+          <img src={img} height={256} width={256}/> { /* some picture */ }
+        </div>
+      </div>
+    </div>
   )
 }
---
+++ src/components/Topbar.tsx
@@ -0,0 +1,19 @@
+export default function Topbar() {
+  return (
+    <div className='flex-1 flex flex-col'>
+      <nav className='px-4 flex justify-between bg-white h-16 border-b-2'>
+        <ul className='flex items-center'>
+          <li className='font-bold font-mono'>
+            Overdose
+          </li>
+        </ul>
+  
+        <ul className='flex items-center'>
+          <li>
+            <a className='text-blue-500 font-bold' href='/login'>로그인</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
