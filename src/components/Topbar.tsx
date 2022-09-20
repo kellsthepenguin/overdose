0d9b93eec3a69d69f807aaa346251c5369cc1255
Kim Jioo <jwkim101201@gmail.com>
2022-09-21 07:00:00 +0900

    add: make logo clickable

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -3,8 +3,8 @@ export default function Topbar() {
     <div className='flex-1 flex flex-col'>
       <nav className='px-4 flex justify-between bg-white h-16 border-b-2'>
         <ul className='flex items-center'>
-          <li className='font-bold font-mono'>
-            Overdose
+          <li>
+            <a className='font-bold font-mono' href='/'>Overdose</a>
           </li>
         </ul>
   
