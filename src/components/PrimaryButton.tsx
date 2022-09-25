1cec17573948ed6db9cc531ddfcfe9f302d8bd8d
Kim Jioo <jwkim101201@gmail.com>
2022-09-25 10:39:44 +0900

    feat: create primary button

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,11 @@
+import { FC, PropsWithChildren } from 'react'
+
+const PrimaryButton: FC<PropsWithChildren> = ({ children }: PropsWithChildren) => {
+  return (
+    <button type='button' className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2 mr-2 mb-2m focus:outline-none'>
+      {children}
+    </button>
+  )
+}
+
+export default PrimaryButton
