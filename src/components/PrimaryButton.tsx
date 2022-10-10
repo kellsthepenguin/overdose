83d331f5841d7e039ef5c7811a1549f41e5c5485
Kim Jioo <jwkim101201@gmail.com>
2022-10-11 07:55:42 +0900

    feat: add href to primarybutton

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,10 +1,10 @@
 import { FC, PropsWithChildren } from 'react'
 
-const PrimaryButton: FC<PropsWithChildren> = ({ children }: PropsWithChildren) => {
+const PrimaryButton: FC<PropsWithChildren<{ href: string }>> = ({ children, href }: PropsWithChildren<{ href: string }>) => {
   return (
-    <button type='button' className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2 mr-2 mb-2m focus:outline-none'>
+    <a href={href} className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2 mr-2 mb-2m focus:outline-none'>
       {children}
-    </button>
+    </a>
   )
 }
 
