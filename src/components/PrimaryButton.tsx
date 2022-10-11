02b0b533540f58d97317f68e3f33f7d666d8d532
Kim Jioo <jwkim101201@gmail.com>
2022-10-11 16:40:57 +0900

    fix: make href optional

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,6 +1,6 @@
 import { FC, PropsWithChildren } from 'react'
 
-const PrimaryButton: FC<PropsWithChildren<{ href: string }>> = ({ children, href }: PropsWithChildren<{ href: string }>) => {
+const PrimaryButton: FC<PropsWithChildren<{ href?: string }>> = ({ children, href }: PropsWithChildren<{ href?: string }>) => {
   return (
     <a href={href} className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2 mr-2 mb-2m focus:outline-none'>
       {children}
