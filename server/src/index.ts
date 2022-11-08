fe7e9b8c314ab3d920a72f307bfb0972f6d7c82b
Kim <jwkim101201@gmail.com>
2022-11-08 17:02:44 +0900

    fix path issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ const app = express()
 const server = createHttpServer(app)
 const io = new IOServer(server)
 
-app.use(express.static(path.join(__dirname, '../../dist/assets')))
+app.use('/assets', express.static(path.join(__dirname, '../../dist/assets')))
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../dist/index.html'))
