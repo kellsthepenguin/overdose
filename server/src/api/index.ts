dda92dd50e2619939e1cf40ab73f45afa11d3649
Kim <jwkim101201@gmail.com>
2022-11-29 12:40:26 +0900

    add key creation api

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,12 +1,15 @@
+import prisma from '../prisma'
 import { Router } from 'express'
 import fetch from 'node-fetch'
+import { generateString } from '../util'
+import { generateSlug } from 'random-word-slugs'
 
 const router = Router()
 
-router.post('/new', (req, res) => {
-  const { captcha, name } = req.body
+router.post('/new', async (req, res) => {
+  const { captcha, type, name } = req.body
 
-  fetch('https://hcaptcha.com/siteverify', {
+  const result = await fetch('https://hcaptcha.com/siteverify', {
     method: 'post',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
@@ -15,7 +18,28 @@ router.post('/new', (req, res) => {
       'response': captcha,
       'secret': process.env.HCAPTCHA_SECRET!
     })
-  }).then((r) => r.json()).then((json) => res.json(json))
+  }).then((r) => r.json())
+
+  if (result.isSuccess) {
+    if (type !== 'cr' || type !== 'mr') {
+      return res.status(500).json({
+        ok: false,
+        reason: 'invalid type'
+      })
+    }
+
+    prisma.user.create({
+      data: {
+        key: type === 'cr' ? generateString(30) : generateSlug(5),
+        name
+      }
+    }).then((user) => {
+      res.json(user)
+    }).catch((err) => {
+      res.status(500).json({ ok: false, reason: 'server error' })
+      console.log(err)
+    })
+  }
 })
 
 export default router
