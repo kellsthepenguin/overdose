6e3187e469d2aed7e755752d2cd45d70261dbbe2
Kim <jwkim101201@gmail.com>
2022-12-17 09:20:27 +0900

    remove unnecessary space

--- server/src/util.ts
+++ server/src/util.ts
@@ -1,6 +1,6 @@
 export function generateString(length: number) {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  let result = ' '
+  let result = ''
   const charactersLength = characters.length
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength))
