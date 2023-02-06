a347e1526b1562ed3a53ab62c75b0087959e7a98
Kim <37768795+kellsthepenguin@users.noreply.github.com>
2023-02-07 07:18:55 +0900

    add register

---
+++ src/pages/api/register.ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import prisma from '@/prisma'
+import { sha256 } from 'js-sha256'
+import { nanoid } from 'nanoid'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id, pw, name } = req.body
+  const salt = nanoid(36)
+
+  if (id && pw && name) {
+    prisma.user.create({
+      data: {
+        id,
+        pw: sha256(pw + salt),
+        salt,
+        name
+      }
+    }).then(() => res.json({ ok: true }))
+      .catch(() => res.json({ ok: false, error: 'db error' }))
+  } else {
+    res.json({ ok: false, error: 'invalid body' })
+  }
+}
