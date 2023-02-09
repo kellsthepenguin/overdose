ebf3363dd1f861414239e654746997eb95a4d629
Kim <37768795+kellsthepenguin@users.noreply.github.com>
2023-02-09 14:00:06 +0900

    change href to onclick

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,8 +1,8 @@
 import { FC, PropsWithChildren } from 'react'
 
-export default function PrimaryButton({ children, href }: PropsWithChildren<{ href?: string }>) {
+export default function PrimaryButton({ children, onClick }: PropsWithChildren<{ onClick?: () => void }>) {
   return (
-    <a href={href} className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2 mr-2 mb-2m focus:outline-none'>
+    <a className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2 mr-2 mb-2m focus:outline-none' onClick={onClick}>
       {children}
     </a>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
           <div>
             <p className='font-xl'>당신의 대화는 보호받아야 하니까.</p>
             <p className='text-5xl font-bold font-mono pb-2'>Overdose.</p>
-            <PrimaryButton href='/new'>
+            <PrimaryButton onClick={() => location.href = '/register'}>
               시작하기 &nbsp; 
               <FontAwesomeIcon icon={faChevronRight} />
             </PrimaryButton>
