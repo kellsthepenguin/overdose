5fff47bfd7b20494e45629671064c8896f295b4a
Kim <37768795+kellsthepenguin@users.noreply.github.com>
2023-02-14 10:18:11 +0900

    add paste warning message

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    console.log('%cHold Up!', 'font-size:50pt;color:red')
+    console.log(
+      '%cIf someone told you to copy/paste something here you have an 11/10 chance you are being scammed.',
+      'font-size:15pt;color:red'
+    )
+    console.log()
+  })
+
   return <Component {...pageProps} />
 }
