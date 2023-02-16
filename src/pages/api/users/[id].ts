de02f9f18248a8aad171d54a93530a071163d2a0
Kim <37768795+kellsthepenguin@users.noreply.github.com>
2023-02-16 17:13:17 +0900

    add user info api

---
+++ src/pages/api/users/[id].ts
@@ -0,0 +1,23 @@
+import prisma from '@/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query as { id: string }
+  const user = (await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  })) as any
+
+  if (!user) {
+    res.json({ ok: false, error: "user dosen't exists" })
+  }
+
+  delete user.pw
+  delete user.salt
+
+  res.json(user)
+}
